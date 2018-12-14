c204816bc4ed058ea71924e7908afa4d2a42b478
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-14 11:10:58 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const config = require("./config/config.json");
+var appInfo = require("./package.json");
+const eStatics = require("e-statics")(app);
+
+app.use(bodyParser.json());
+app.use(
+  morgan(
+    ":remote-addr :method :url :status :res[content-length] - :response-time ms"
+  )
+);
+app.use(eStatics.counter);
+app.use("/api", require("./lib"));
+const port = process.env.PORT || config.port || 8080;
+app.listen(port, () => {
+  console.log("Listening to port: %i", port);
+});
